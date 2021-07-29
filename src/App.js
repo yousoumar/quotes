14b8e7b62991d2d1b3356a94fa9586eb8e6f64a5
yousoumar <oumaiyr@gmail.com>
2021-07-29 15:44:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
+
+import { useState, useEffect} from 'react';
+import bg from './bg.jpg'
 import './App.css';
 
 function App() {
+  
+  document.body.style.backgroundImage = `url(${bg})`;
+
+  const [quoteState, setQuoteState] = useState('{text : "The way we communicate with others and with ourselves ultimately determines the quality of our lives.", author : "Tony Robbins"}');
+  const [quotesState, quotesSetState] = useState('');
+  
+  
+  useEffect(() => {
+      fetch("https://type.fit/api/quotes")
+      .then(response => response.json())
+      .then(data => {
+        quotesSetState(data)
+        setQuoteState(data[Math.floor(data.length*Math.random())])
+        
+      })
+    }
+  , []);
+
+
+  function generateNewQuote(){
+    setQuoteState(quotesState[Math.floor(Math.random()*quotesState.length)]);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      
+      <p className = "quote">"{quoteState.text}"</p>
+      <p className = "author">by {quoteState.author ? quoteState.author : "Unknown author"}</p>
+      <button 
+        className = "new-quote-button"
+        onClick ={generateNewQuote}
+      >
+        New quote
+      </button>
     </div>
   );
 }
