d340cde99aee0447917a7ecfff2755e3077878cd
yousoumar <oumaiyr@gmail.com>
2021-07-29 22:19:20 +0200

    optimization and code review

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-<img src ="./screenshot.png"></img>
+Ceci est un générateur des citations codé avec `React`. Les citations proviennent de l'`API` de `Type.fit`. 
+
+L'application signalera au visiteur et lui demandera de revenir plus tard au cas où l'appel ne se passe pas bien.
+
+Vous pouvez visiter en cliquant sur l'image ci-dessous ou sur le lien en bas de la section `About`
+
+<a href = "https://yousoumar.github.io/quotes/"><img src ="./screenshot.png"></img></a>
+
 
---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import { useState, useEffect} from 'react';
+import bg from '../assets/bg.jpg'
+import './App.css';
+
+function App() {
+  
+  document.body.style.backgroundImage = `url(${bg})`;
+
+  const [quoteState, setQuoteState] = useState(
+      { text : "The way we communicate with others and with ourselves ultimately determines the quality of our lives.", 
+        author : "Tony Robbins"
+      }
+    );
+  const [quotesState, quotesSetState] = useState([]);
+  console.log(quotesState);
+  
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+        .then(response => response.json())
+        .then(data => {
+          quotesSetState(data);
+          
+        })
+        .catch(()=>{
+          setQuoteState({text : "We have issues with our database. Come back later :)", author : "The Developper"})
+        })
+   
+    }
+  , []);
+  function generateNewQuote(){
+    if(quotesState.length > 0 ){
+      setQuoteState(quotesState[Math.floor(Math.random()*quotesState.length)]);
+    }
+    
+
+  }
+  return (
+    <div id="app">
+      <div className="quote-author">
+        <p className = "quote">"{quoteState.text}"</p>
+        <p className = "author">by {quoteState.author ? quoteState.author : "Unknown author"}</p>
+      </div>
+
+      <button 
+        className = "new-quote-button"
+        onClick ={generateNewQuote}
+      >
+        New quote
+      </button>
+    </div>
+  );
+}
+
+export default App;
